4eb14d70324bbd70343b18d92a97b23395fbdee8
tonystarkmk1 <DOMENICOSTANGONI@GMAIL.COM>
2026-07-01 20:23:03 +0200

    Fix safe wrapper newline replacement

--- scripts/patch-pwa-localita-safe.js
+++ scripts/patch-pwa-localita-safe.js
@@ -4,6 +4,6 @@ const path = require('path');
 const source = path.join(__dirname, 'patch-pwa-localita.js');
 const target = path.join(__dirname, '.patch-pwa-localita-runtime.js');
 let code = fs.readFileSync(source, 'utf8');
-code = code.replace('}\nhtml = html.replace("class=\\"btn ${id===\'base\'?\'gold\':\'teal\'}\\"", "class=\\"btn teal\\"");', '}\nhtml = html.replace("class=\\"btn ${id===\'base\'?\'gold\':\'teal\'}\\"", "class=\\"btn teal\\"");');
+code = code.replace('}\\nhtml = html.replace("class=\\"btn ${id===\'base\'?\'gold\':\'teal\'}\\"", "class=\\"btn teal\\"");', '}\nhtml = html.replace("class=\\"btn ${id===\'base\'?\'gold\':\'teal\'}\\"", "class=\\"btn teal\\"");');
 fs.writeFileSync(target, code);
 require(target);
