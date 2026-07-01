b486adc577ea6aab7f1615a5dadccf9fa5c6832c
tonystarkmk1 <DOMENICOSTANGONI@GMAIL.COM>
2026-07-02 00:54:43 +0200

    Cache PNG PWA icons

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'home-care-pwa-v12';
-const CORE_ASSETS = ['/', '/offline.html', '/manifest.json', '/icon.svg', '/icon-192.svg', '/icon-512.svg'];
+const CACHE_NAME = 'home-care-pwa-v13';
+const CORE_ASSETS = ['/', '/offline.html', '/manifest.json', '/icon.svg', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).catch(() => null));
