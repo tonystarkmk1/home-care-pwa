19cfb7a864ec567b1ad6a64390c5a540c25dd085
tonystarkmk1 <DOMENICOSTANGONI@GMAIL.COM>
2026-07-01 13:15:38 +0200

    Add service worker

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'home-care-pwa-v1';
+const CORE_ASSETS = ['/', '/manifest.json', '/icon.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).catch(() => null));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith('/api/') || event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then((res) => res || caches.match('/'))));
+});
