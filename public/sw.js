aa134ce1e30048fd80f99f46378a934cb70dc0f1
tonystarkmk1 <DOMENICOSTANGONI@GMAIL.COM>
2026-07-01 14:24:55 +0200

    Refresh PWA cache for registration UI

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'home-care-pwa-v1';
-const CORE_ASSETS = ['/', '/manifest.json', '/icon.svg'];
+const CACHE_NAME = 'home-care-pwa-v3';
+const CORE_ASSETS = ['/manifest.json', '/icon.svg'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).catch(() => null));
@@ -16,5 +16,13 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
   if (url.pathname.startsWith('/api/') || event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then((res) => res || caches.match('/'))));
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => null);
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((res) => res || caches.match('/')))
+  );
 });
